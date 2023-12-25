0b3d2fa4c12225c3092a63af3b843c871e2b18ee
TianyiZhang2 <43654139+TianyiZhang2@users.noreply.github.com>
2023-12-25 14:34:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-yvwbwm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-yvwbwm)
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  /* TODO: NgForm what do? */
+  submitForm(event: NgForm){
+    console.log(event);
+  }
+}
